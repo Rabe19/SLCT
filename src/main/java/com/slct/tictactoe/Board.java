11a11ec758408988fb7f0fa969e95ba8b622b852
alex <alexander.haschberger98@gmail.com>
2026-06-11 19:08:16 +0200

    US-01: implement move placement

---
+++ src/main/java/com/slct/tictactoe/Board.java
@@ -0,0 +1,43 @@
+package com.slct.tictactoe;
+
+public class Board {
+
+    private final Player[][] cells;
+
+    public Board() {
+        this.cells = new Player[3][3];
+    }
+
+    public boolean makeMove(int row, int column, Player player) {
+        if (!isValidPosition(row, column)) {
+            return false;
+        }
+
+        if (!isCellEmpty(row, column)) {
+            return false;
+        }
+
+        cells[row][column] = player;
+        return true;
+    }
+
+    public boolean isCellEmpty(int row, int column) {
+        if (!isValidPosition(row, column)) {
+            return false;
+        }
+
+        return cells[row][column] == null;
+    }
+
+    public Player getCell(int row, int column) {
+        if (!isValidPosition(row, column)) {
+            return null;
+        }
+
+        return cells[row][column];
+    }
+
+    private boolean isValidPosition(int row, int column) {
+        return row >= 0 && row < 3 && column >= 0 && column < 3;
+    }
+}
