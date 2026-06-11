62fdf0d02886c661c0eec858d81fab95b8dbe84f
alex <alexander.haschberger98@gmail.com>
2026-06-11 21:02:09 +0200

    Implement tic tac toe game logic

--- src/main/java/com/slct/tictactoe/Main.java
+++ src/main/java/com/slct/tictactoe/Main.java
@@ -1,17 +1,51 @@
 package com.slct.tictactoe;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Board board = new Board();
+        Player currentPlayer = Player.X;
+
+        System.out.println("Tic Tac Toe");
+        System.out.println("Gib Zeile und Spalte von 0 bis 2 ein.");
+
+        while (board.getGameState() == GameState.RUNNING) {
+            System.out.println();
+            System.out.println(board.display());
+            System.out.println("Spieler " + currentPlayer + " ist dran.");
+
+            System.out.print("Zeile: ");
+            int row = scanner.nextInt();
+
+            System.out.print("Spalte: ");
+            int column = scanner.nextInt();
+
+            boolean moveWasSuccessful = board.makeMove(row, column, currentPlayer);
+
+            if (!moveWasSuccessful) {
+                System.out.println("Ungültiger Zug. Bitte nochmal versuchen.");
+                continue;
+            }
+
+            currentPlayer = currentPlayer == Player.X ? Player.O : Player.X;
         }
+
+        System.out.println();
+        System.out.println(board.display());
+
+        GameState result = board.getGameState();
+
+        if (result == GameState.X_WON) {
+            System.out.println("Spieler X hat gewonnen!");
+        } else if (result == GameState.O_WON) {
+            System.out.println("Spieler O hat gewonnen!");
+        } else {
+            System.out.println("Unentschieden!");
+        }
+
+        scanner.close();
     }
 }
